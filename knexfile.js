b38fe3c82dea9f5e7ffc064dfd9c83888cff4f1c
Jennifer Woodson <jenniferpwoodson@gmail.com>
2018-01-06 07:01:10 -0700

    Add production and test to knexfile and add Procfile

--- knexfile.js
+++ knexfile.js
@@ -9,5 +9,27 @@ module.exports = {
       directory: './db/seeds/dev'
     },
     useNullAsDefault: true
+  },
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/mental_health_app_test',
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
+    },
+    useNullAsDefault: true
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL + `?ssl=true`,
+    migrations: {
+      directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/production'
+    },
+    useNullAsDefault: true
   }
 };
