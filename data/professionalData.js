195cd041f7793879da9bd8066a882cd541ed46e0
Jennifer Woodson <jenniferpwoodson@gmail.com>
2018-01-08 15:20:48 -0700

    Add email address for professionals

--- data/professionalData.js
+++ data/professionalData.js
@@ -2,52 +2,62 @@ const professionals = [
   {
     professional_name: "Dr. Smith",
     professional_location: "Denver",
-    professional_phone: "555-555-1234"
+    professional_phone: "555-555-1234",
+    professional_email: "smith@email.com"
   },
   {
     professional_name: "Dr. Johnson",
     professional_location: "Denver",
-    professional_phone: "555-555-1111"
+    professional_phone: "555-555-1111",
+    professional_email: "johnson@email.com"
   },
   {
     professional_name: "Dr. Williams",
     professional_location: "Denver",
-    professional_phone: "555-555-1212"
+    professional_phone: "555-555-1212",
+    professional_email: "williams@email.com"
   },
   {
     professional_name: "Dr. Patel",
     professional_location: "Denver",
-    professional_phone: "555-555-1233"
+    professional_phone: "555-555-1233",
+    professional_email: "patel@email.com"
   },
   {
     professional_name: "Dr. Miller",
     professional_location: "Denver",
-    professional_phone: "555-555-1293"
+    professional_phone: "555-555-1293",
+    professional_email: "miller@email.com"
   },
   {
     professional_name: "Dr. Jones",
     professional_location: "Denver",
-    professional_phone: "555-555-6677"
+    professional_phone: "555-555-6677",
+    professional_email: "jones@email.com"
   },
   {
     professional_name: "Dr. Hall",
     professional_location: "Denver",
-    professional_phone: "555-555-1234"
+    professional_phone: "555-555-1234",
+    professional_email: "hall@email.com"
   },
   {
     professional_name: "Dr. Adams",
     professional_location: "Denver",
-    professional_phone: "555-555-1234"
+    professional_phone: "555-555-1234",
+    professional_email: "adams@email.com"
   },
   {
     professional_name: "Dr. Zimmerman",
     professional_location: "Denver",
-    professional_phone: "555-555-1234"
+    professional_phone: "555-555-1234",
+    professional_email: "zimmerman@email.com"
   },
   {
     professional_name: "Dr. Wallace",
     professional_location: "Denver",
-    professional_phone: "555-555-1234"
+    professional_phone: "555-555-1234",
+    professional_email: "wallace@email.com"
   }
 ];
 
